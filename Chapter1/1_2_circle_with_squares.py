e696a1ad67b501faca16e737bfb9c9233349001b
doug ferreira <ferreira.doug.w@gmail.com>
2023-02-22 10:33:48 -0700

    Add files via upload

---
+++ Chapter1/1_2_circle_with_squares.py
@@ -0,0 +1,13 @@
+from turtle import *
+shape('turtle')
+speed(0)
+
+
+def square_loop():
+    for i in range(60):
+        forward(100)
+        right(90)
+        right(5)
+       
+
+square_loop()
