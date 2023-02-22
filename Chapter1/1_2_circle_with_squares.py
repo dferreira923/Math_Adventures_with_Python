384f17d79a5ae0e274a02b1ab835a20faa5e5d45
doug ferreira <ferreira.doug.w@gmail.com>
2023-02-22 10:34:15 -0700

    Update 1_2_circle_with_squares.py

--- Chapter1/1_2_circle_with_squares.py
+++ Chapter1/1_2_circle_with_squares.py
@@ -2,12 +2,10 @@ from turtle import *
 shape('turtle')
 speed(0)
 
-
 def square_loop():
     for i in range(60):
         forward(100)
         right(90)
         right(5)
        
-
 square_loop()
